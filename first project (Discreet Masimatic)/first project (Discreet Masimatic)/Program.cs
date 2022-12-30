51569cc6d33db61df551b3f6d50f937dfe5212b8
OmaarMostafaa <97802359+OmaarMostafaa@users.noreply.github.com>
2022-12-30 12:14:16 +0200

    Add files via upload

---
+++ first project (Discreet Masimatic)/first project (Discreet Masimatic)/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+    namespace HelloWorld
+   {
+       class Program
+       {
+           static void Main(string[] args)          
+        {
+int y;
+int x;
+int z;
+Console.WriteLine("Max Num");
+x = int.Parse(Console.ReadLine());
+z = int.Parse(Console.ReadLine());
+Console.WriteLine("Min Num");
+for (y = int.Parse(Console.ReadLine()); z <= x; y++) ;
+            {
+                for (z = 2; z <= y; z++)
+                    if (y%z==0)
+                    {
+
+                        break;
+                    }
+       if (y == z)
+                {
+                    Console.WriteLine(y);
+                }
+            }
+        }
+     }
+ }
